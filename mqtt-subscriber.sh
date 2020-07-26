a3f6d232c9e40949408c71532178d422f25c71f2
neosilviu <neosilviu@gmail.com>
2020-07-26 17:56:49 +0300

    Update mqtt-subscriber.sh

--- mqtt-subscriber.sh
+++ mqtt-subscriber.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-MQTT_SERVER=`cat /home/hass/inverter/mqtt.json | jq '.server' -r`
-MQTT_PORT=`cat /home/hass/inverter/mqtt.json | jq '.port' -r`
-MQTT_TOPIC=`cat /home/hass/inverter/mqtt.json | jq '.topic' -r`
-MQTT_DEVICENAME=`cat /home/hass/inverter/mqtt.json | jq '.devicename' -r`
-MQTT_USERNAME=`cat /home/hass/inverter/mqtt.json | jq '.username' -r`
-MQTT_PASSWORD=`cat /home/hass/inverter/mqtt.json | jq '.password' -r`
+MQTT_SERVER=`cat ~/inverter/mqtt.json | jq '.server' -r`
+MQTT_PORT=`cat ~/inverter/mqtt.json | jq '.port' -r`
+MQTT_TOPIC=`cat ~/inverter/mqtt.json | jq '.topic' -r`
+MQTT_DEVICENAME=`cat ~/inverter/mqtt.json | jq '.devicename' -r`
+MQTT_USERNAME=`cat ~/inverter/mqtt.json | jq '.username' -r`
+MQTT_PASSWORD=`cat ~/inverter/mqtt.json | jq '.password' -r`
 
 while read rawcmd;
 do
 
     echo "Incoming request send: [$rawcmd] to inverter."
-    /home/hass/inverter/cli/inverter_poller -r $rawcmd;
+    ~/inverter/cli/inverter_poller -r $rawcmd;
 
 done < <(mosquitto_sub -h $MQTT_SERVER -p $MQTT_PORT -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t "$MQTT_TOPIC/sensor/$MQTT_DEVICENAME" -q 1)
